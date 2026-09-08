fdac81acd2f134cf80acd9166fcbca09f5f622ca
chrisgeosonlg <christopherlusiga@gmail.com>
2026-09-08 22:14:56 +0300

    Set hero slideshow interval to four seconds

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,7 @@ export default function Home() {
     if (!isPlaying || isHoverPaused) return undefined
     const timer = window.setInterval(() => {
       setActiveSlide((current) => (current + 1) % heroSlides.length)
-    }, 6500)
+    }, 4000)
     return () => window.clearInterval(timer)
   }, [isPlaying, isHoverPaused])
 
